b543b17bbf3be339cc73c65283580b9d60e3bf24
ibmabhishek <erren98@gmail.com>
2021-03-29 16:45:02 +0530

    bring all code to a single folder

---
+++ demo/src/main/java/com/ibm/demo/OrderController.java
@@ -0,0 +1,77 @@
+package com.ibm.demo;
+
+import java.util.List;
+import java.util.Optional;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ibm.demo.entity.Order;
+import com.ibm.demo.service.OrderService;
+
+@RestController // Bean
+public class OrderController { // Fronted
+
+	@Autowired // is used for Dependency Injection
+	OrderService orderService; // Dependency Injection
+	
+	@PostMapping("/order")
+	@ResponseStatus(code = HttpStatus.CREATED)
+	String createOrder(@RequestBody @Valid Order order, BindingResult bindingResult) {
+		validateModel(bindingResult);
+		System.out.println(order);
+		return orderService.createOrder(order); // Delegation of object to Service Class
+	}
+	
+	
+	
+	private void validateModel(Errors bindingResult) {
+		if (bindingResult.hasErrors()) {
+			throw new IllegalArgumentException("Something went wrong, Please retry !!");
+		}
+
+	}
+
+	@PutMapping("/order/{id}")
+	void updateOrder(@RequestBody @Valid Order order, BindingResult bindingResult, @PathVariable("id") String orderId) {
+		validateModel(bindingResult);
+		order.setId(orderId);
+		orderService.updateOrder(order);
+	}
+	
+//	@GetMapping("/order")
+//	List<Order> getOrders(){
+//		return orderService.getOrders();
+//		
+//	} 
+//	
+//	/**
+//	 * method to search for an order
+//	 * @param orderId
+//	 * @return zero or matching order
+//	 */
+//
+//
+//	@GetMapping("/order/{id}")
+//	Optional<Order> getOrder(@PathVariable("id") String orderId) {
+//		return orderService.getOrder(orderId);
+//	}
+
+	@DeleteMapping("/order/{id}")
+	void deleteOrder(@PathVariable("id") String orderId) {
+		orderService.deleteOrder(orderId);
+	}
+
+}
