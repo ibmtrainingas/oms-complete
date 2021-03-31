739a6650ad7be5d2e704798fa178b68a6841c1e0
ibmabhishek <erren98@gmail.com>
2021-03-31 10:06:27 +0530

    Put annotation in main class

--- demo/src/main/java/com/ibm/demo/SpringbootDemo1Application.java
+++ demo/src/main/java/com/ibm/demo/SpringbootDemo1Application.java
@@ -5,9 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.mongodb.MongoTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableTransactionManagement
 public class SpringbootDemo1Application {
 
 	public static void main(String[] args) {
